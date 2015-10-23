cf0e814a8386222e7bbefa002080a92a68a3282a
Christopher Stock <email@christopherstock.de>
2015-10-23 14:49:58 +0200

    Moved all JS code to TS.

--- src/de/mayflower/angular/MfgNgApp.ts
+++ src/de/mayflower/angular/MfgNgApp.ts
@@ -2,17 +2,30 @@
     /************************************************************************************
     *   The main class contains the application's points of entry and termination.
     *
+    *   TODO ASAP   Try .d.ts !
+    *   TODO HIGH   Outsource all css style attributes.
+    *   TODO INIT   Move all JS files to TS.
+    *   TODO LOW    Separate functionality to different TS classes.
+    *   TODO WEAK   Give TS modules one more try?
+    *
+    *   DONE        Apply AngularJS animations.
+    *   DONE        Solve all functionality from the AngularJS tutorial.
+    *
     *   @author     Christopher Stock
     *   @version    0.0.1
     *****************************************************************************/
     class MfgNgApp
     {
+        public      static          myModule            :any            = null;
+
         /*****************************************************************************
         *   This method is invoked when the application starts.
         *****************************************************************************/
         public static main():void
         {
-            alert( "TypeScript is onboard!" );
+
+
+
         }
     }
 
@@ -31,3 +44,121 @@
     window.onunload = function()
     {
     };
+
+
+
+    /*****************************************************************************
+    *   Angular code to move to TS.
+    *****************************************************************************/
+    MfgNgApp.myModule = angular.module(
+        'chrisApp',
+        [
+            'ngAnimate',
+            'ngRoute'
+        ]
+    );
+
+    console.log( "myModule is: [" + MfgNgApp.myModule + "]" );
+
+    MfgNgApp.myModule.controller(
+        'ArticlesCtrl',
+        function( $scope ) {
+
+            $scope.articles = [
+                {   id: 7,  name: "Pizza Vegetaria", price: 5    },
+                {   id: 13, name: "Pizza Salami",    price: 5.5  },
+                {   id: 19, name: "Pizza Thunfisch", price: 6    }
+            ];
+        }
+    );
+
+    MfgNgApp.myModule.controller(
+        'FlyerArticlesCtrl',
+        function( $scope, $http ) {
+
+            $http.get( 'res/data/articles.json' ).then(
+
+                function( articlesResponse ) {
+
+                    $scope.flyerArticles = articlesResponse.data;
+                }
+            );
+        }
+    );
+
+    MfgNgApp.myModule.factory(
+        'Cart',
+        function() {
+
+            var items = [];
+            return {
+
+                getItems: function() {
+
+                    return items;
+                },
+                addArticle: function( article ) {
+
+                    items.push( article );
+                },
+                sum: function() {
+
+                    return items.reduce(
+                        function( total, article ) {
+                            return total + article.price;
+                        },
+                        0
+                    );
+                }
+            };
+        }
+    );
+
+    MfgNgApp.myModule.controller(
+        'CartArticlesCtrl',
+        function( $scope, $http, Cart ) {
+
+            $scope.cart = Cart;
+
+            $http.get( 'res/data/cartArticles.json' ).then(
+                function( articlesResponse ) {
+                    $scope.cartArticles = articlesResponse.data;
+                }
+            );
+        }
+    );
+
+    MfgNgApp.myModule.controller(
+        'CartCartCtrl',
+        function( $scope, Cart ) {
+
+            $scope.cart = Cart;
+        }
+    );
+
+    MfgNgApp.myModule.directive(
+        'price',
+        function() {
+
+            return {
+                restrict:   'E',
+                scope:      {
+                    value:  '='
+                },
+                template:       '<span ng-show="value == 0">kostenlos</span>'
+                            +   '<span ng-show="value > 0">{{value | currency}}</span>'
+            }
+        }
+    );
+
+    MfgNgApp.myModule.config(
+        function( $routeProvider ) {
+
+            $routeProvider
+                .when( '/',         { template:     'Willkommen zur Startseite' } )
+                .when( '/about',    { template:     'Über unsere Pizzeria'      } )
+                .when( '/contact',  { templateUrl:  'res/html/contact.html'     } )
+                .when( '/imprint',  { template:     'Impressums-Angaben'        } )
+                .otherwise(         { redirectTo:   '/'                         } );
+        }
+    );
