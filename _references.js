44e9d132d3461a287d5bf16943d74ddb0461a1c8
Christopher Stock <email@christopherstock.de>
2015-10-23 15:11:37 +0200

    Added TypeScript type definiton file for AngularJS.

--- _references.js
+++ _references.js
@@ -1,2 +1,4 @@
+/// <reference path="lib/angular_1.4.0.d.ts" />
 /// <reference path="src/de/mayflower/angular/MfgNgApp.ts" />
+/// <reference path="src/de/mayflower/angular/MfgNgTest.ts" />
 //# sourceMappingURL=_references.js.map
--- _references.ts
+++ _references.ts
@@ -1,2 +1,5 @@
 
+/// <reference path="lib/angular_1.4.0.d.ts" />
+
 /// <reference path="src/de/mayflower/angular/MfgNgApp.ts" />
+/// <reference path="src/de/mayflower/angular/MfgNgTest.ts" />
